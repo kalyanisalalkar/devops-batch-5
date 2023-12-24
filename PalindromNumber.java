993395cd32b18279c586098a18ab8c5b10925cbf
kalyanisalalkar <kalyanisallalkar@gmail.com>
2023-12-24 09:23:03 +0530

    added palindrom number

---
+++ PalindromNumber.java
@@ -0,0 +1,13 @@
+class PalindromNumber{
+	public static void main(String[] agrs){
+		int number = 123454678,rev=0,rem;
+
+		while(number>nmuber){
+			rem = number%10;
+			rev = (rev*10)+rem;
+			number = number /10;
+
+		}
+		System.out.println(rev);
+	}
+}	
