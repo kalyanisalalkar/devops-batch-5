bbd94265e1b5fdbcddb028621065d86f85a75c9e
kalyanisalalkar <kalyanisallalkar@gmail.com>
2023-12-25 11:16:44 +0530

    palindromNumber code

--- PalindromNumber.java
+++ PalindromNumber.java
@@ -1,6 +1,7 @@
 class PalindromNumber{
 	public static void main(String[] agrs){
 		int number = 123454678,rev=0,rem;
+		int temp = number;
 
 		while(number>nmuber){
 			rem = number%10;
@@ -9,5 +10,15 @@ class PalindromNumber{
 
 		}
 		System.out.println(rev);
+
+		
+		if(temp == number){
+			System.out.println("Number Are Palindrom");
+
+		}
+		else{
+			System.out.println("Number Not Palindrom");
+		}
+
 	}
 }	
